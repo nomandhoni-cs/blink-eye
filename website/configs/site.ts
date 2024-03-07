4c8bd23b50772ad8b13de964b85e358257838859
nomandhoni-cs <nomandhoni@yahoo.com>
2024-03-07 20:06:18 +0600

    Changed tinny url related error in sitemap

--- website/configs/site.ts
+++ website/configs/site.ts
@@ -4,7 +4,7 @@ export const CONFIG = {
   repository: "https://github.com/nomandhoni-cs/blink-eye",
   documentation: "https://github.com/nomandhoni-cs/blink-eye",
   nomandhoni: "https://twitter.com/nomandhoni",
-  website: "https://showwand.vercel.app",
+  website: "https://blinkeye.vercel.app",
   buymecoffee: "https://www.buymeacoffee.com/nomandhoni",
   command:
     "pyinstaller --name BlinkEye --onefile --windowed --icon=blink-eye-logo.ico --hidden-import plyer.platforms.win.notification blink-eye.py",
