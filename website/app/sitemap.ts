1112f5c9955b7dbae47b93d8bc0d49e89e82c56b
nomandhoni-cs <nomandhoni@yahoo.com>
2024-03-07 19:48:26 +0600

    Updated the sitemap

--- website/app/sitemap.ts
+++ website/app/sitemap.ts
@@ -2,44 +2,24 @@ import { SEO } from "@/configs/seo";
 import { MetadataRoute } from "next";
 
 const sitemap = (): MetadataRoute.Sitemap => {
-  return [
-    {
-      url: SEO.url,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
-    },
-    {
-      url: SEO.url + "/goodbye",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
-    },
-    {
-      url: SEO.url + "/about",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
-    },
-    {
-      url: SEO.url + "/features",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
-    },
-    {
-      url: SEO.url + "/howtouse",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
-    },
-    {
-      url: SEO.url + "/contribute",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
-    },
+  const routes = [
+    "",
+    "/goodbye",
+    "/about",
+    "/features",
+    "/howtouse",
+    "/contribute",
+    "/privacy",
   ];
+
+  const sitemapUrls: MetadataRoute.Sitemap = routes.map((route) => ({
+    url: `${SEO.url}${route}`,
+    lastModified: new Date(),
+    changeFrequency: "monthly",
+    priority: 1,
+  }));
+
+  return sitemapUrls;
 };
 
 export default sitemap;
