97f5dd8b54bc21a2e52c9d71e31031c219d719c4
Moheshwar Amarnath Biswas <fluentmoheshwar@outlook.com>
2024-04-01 22:29:12 +0600

    (build) added linux build script

---
+++ build_fedora.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+ver="1.4.0"
+
+echo Installing Dependencies...
+sudo dnf install -y rpmdevtools rpmlint python3-devel python3-pip python3-tkinter
+pip install wheel
+pip install -r REQUIREMENTS.txt
+pip install pyinstaller
+echo Building the executable...
+pyinstaller --name BlinkEye --onefile --windowed --hidden-import plyer.platforms.linux.notification blink-eye.py
+echo Building the installer...
+rpmdev-setuptree
+cp -r ./* ~/rpmbuild/BUILD
+rpmbuild -bb ./BlinkEye_Fedora.spec
+rm -rf ~/rpmbuild/BUILD
+mv ~/rpmbuild/RPMS/x86_64/BlinkEye-$ver-1.fc39.x86_64.rpm ./ExecutableFile/BlinkEye-$ver-1.fc39.x86_64.rpm
