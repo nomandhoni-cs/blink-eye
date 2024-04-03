4019762f7969dc210e27c36e23104752882d41f4
Nusab Taha <nusabtaha33@gmail.com>
2024-04-03 19:04:18 +0600

    Fix file paths and add license file to Fedora build script

--- application/build_fedora.sh
+++ application/build_fedora.sh
@@ -8,10 +8,12 @@ pip install wheel
 pip install -r REQUIREMENTS.txt
 pip install pyinstaller
 echo Building the executable...
-pyinstaller --name BlinkEye --onefile --windowed --add-data="./Assets/*;./Assets" --hidden-import plyer.platforms.linux.notification --clean blink-eye.py
+pyinstaller --name BlinkEye --onefile --windowed --add-data="./Assets/*:./Assets" --hidden-import plyer.platforms.linux.notification --clean blink_eye.py
 echo Building the installer...
 rpmdev-setuptree
 cp -r ./* ~/rpmbuild/BUILD
+cp ../LICENSE.txt ~/rpmbuild/BUILD/LICENSE.txt
 rpmbuild -bb ./BlinkEye_Fedora.spec
 rm -rf ~/rpmbuild/BUILD
+mkdir -p ./ExecutableFile/
 mv ~/rpmbuild/RPMS/x86_64/BlinkEye-$ver-1.fc39.x86_64.rpm ./ExecutableFile/BlinkEye-$ver-1.fc39.x86_64.rpm
