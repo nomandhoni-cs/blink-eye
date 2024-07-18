1e3ebdb0b60b09e2eb76fec178429d4ce2577254
Sayad Uddin Tahsin <newhorizon984@gmail.com>
2024-07-18 15:07:35 +0600

    Modified the build commands

--- application/build_windows.bat
+++ application/build_windows.bat
@@ -7,6 +7,6 @@ python -m pip install wheel
 python -m pip install -r REQUIREMENTS.txt
 python -m pip install pyinstaller
 echo Building the executable...
-pyinstaller --name BlinkEye --onefile --windowed --icon="./Assets/blink-eye-logo.ico" --add-data="./Assets/*;./Assets" --add-data="./data/*;./data" --clean blink-eye.py
+pyinstaller --name BlinkEye --onefile --windowed --icon="./Assets/blink-eye-logo.ico" --add-data="./Assets/*;./Assets" --add-data="./data/*;./data" --clean main.py
 echo Building the installer...
 "%ProgramFiles(x86)%\Inno Setup 6\ISCC.exe" BuildFileForInnoSetup.iss
