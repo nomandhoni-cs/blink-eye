5840394baf56eb498f41c55a9e9c68042480689c
Noman Dhoni <nomandhoni@yahoo.com>
2024-09-27 15:16:11 +0600

    Commented unnecessary files

--- application/src/hooks/setup.ts
+++ application/src/hooks/setup.ts
@@ -1,43 +1,43 @@
-import { useEffect, useState } from "react";
-import { setupGlobalShortcuts } from "../app-api/setupShortcuts";
-import { setupTray } from "../app-api/setupTray";
-import {
-  NotificationAPI,
-  setupNotifications,
-} from "../app-api/setupNotifications";
-import { Menu, MenuItem, type MenuItemOptions } from "@tauri-apps/api/menu";
+// import { useEffect, useState } from "react";
+// import { setupGlobalShortcuts } from "../app-api/setupShortcuts";
+// import { setupTray } from "../app-api/setupTray";
+// import {
+//   NotificationAPI,
+//   setupNotifications,
+// } from "../app-api/setupNotifications";
+// import { Menu, MenuItem, type MenuItemOptions } from "@tauri-apps/api/menu";
 
-export function useTauri(menuItems: MenuItemOptions[]) {
-  const [tray, setTray] = useState<null | Menu>(null);
-  const [notifications, setNotifications] = useState<Awaited<NotificationAPI>>({
-    send: async () => console.log("notifications not initialized"),
-    permissionGranted: false,
-  });
+// export function useTauri(menuItems: MenuItemOptions[]) {
+//   const [tray, setTray] = useState<null | Menu>(null);
+//   const [notifications, setNotifications] = useState<Awaited<NotificationAPI>>({
+//     send: async () => console.log("notifications not initialized"),
+//     permissionGranted: false,
+//   });
 
-  useEffect(() => {
-    async function effectUsed() {
-      const initialTray = await setupTray({ tooltip: "Blink Eye" });
-      setTray(initialTray);
+//   useEffect(() => {
+//     async function effectUsed() {
+//       const initialTray = await setupTray({ tooltip: "Blink Eye" });
+//       setTray(initialTray);
 
-      for (const menuItem of menuItems) {
-        const item = await MenuItem.new(menuItem);
-        initialTray.append(item);
-      }
+//       for (const menuItem of menuItems) {
+//         const item = await MenuItem.new(menuItem);
+//         initialTray.append(item);
+//       }
 
-      await setupGlobalShortcuts();
-    }
-    effectUsed();
-  }, []);
+//       await setupGlobalShortcuts();
+//     }
+//     effectUsed();
+//   }, []);
 
-  useEffect(() => {
-    async function effectUsed() {
-      if (!notifications.permissionGranted) {
-        const notify = await setupNotifications();
-        setNotifications(notify);
-      }
-    }
-    effectUsed();
-  }, [notifications.permissionGranted]);
+//   useEffect(() => {
+//     async function effectUsed() {
+//       if (!notifications.permissionGranted) {
+//         const notify = await setupNotifications();
+//         setNotifications(notify);
+//       }
+//     }
+//     effectUsed();
+//   }, [notifications.permissionGranted]);
 
-  return { tray, notifications };
-}
+//   return { tray, notifications };
+// }
