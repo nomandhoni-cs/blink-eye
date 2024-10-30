3da078af4873a9977cf755e4cb0ebe18e637ccd3
Noman Dhoni <nomandhoni@yahoo.com>
2024-10-30 17:25:03 +0600

    Tray icon added

--- src/components/TrayIconComponent.tsx
+++ src/components/TrayIconComponent.tsx
@@ -1,37 +1,46 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { TrayIcon } from "@tauri-apps/api/tray";
 import { Menu } from "@tauri-apps/api/menu";
 import { defaultWindowIcon } from "@tauri-apps/api/app";
 
 const TrayIconComponent: React.FC = () => {
+  const [isTrayIconCreated, setIsTrayIconCreated] = useState(false);
+
   useEffect(() => {
     const initializeTray = async () => {
-      function onTrayMenuClick(itemId: string) {
-        // itemId === 'quit'
-        console.log("Tray menu item clicked: ", itemId);
+      if (!isTrayIconCreated) {
+        const menu = await Menu.new({
+          items: [
+            {
+              id: "dashboard",
+              text: "Dashboard",
+              action: () => {
+                console.log("Dashboard pressed");
+              },
+            },
+            {
+              id: "quit",
+              text: "Quit",
+              action: () => {
+                console.log("quit pressed");
+              },
+            },
+          ],
+        });
+
+        const options = {
+          icon: await defaultWindowIcon(),
+          menu,
+          menuOnLeftClick: true,
+          tooltip: "Blink Eye",
+        };
+        await TrayIcon.new(options);
+        setIsTrayIconCreated(true);
       }
-
-      const menu = await Menu.new({
-        items: [
-          {
-            id: "quit",
-            text: "Quit",
-            action: onTrayMenuClick,
-          },
-        ],
-      });
-
-      const options = {
-        icon: await defaultWindowIcon(),
-        menu,
-        menuOnLeftClick: true,
-      };
-
-      await TrayIcon.new(options);
     };
 
     initializeTray();
-  }, []);
+  }, [isTrayIconCreated]);
 
   return null;
 };
