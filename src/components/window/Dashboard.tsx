c68aac3f447c54c7cd7a8f57502aa83e6c969597
Noman Dhoni <nomandhoni@yahoo.com>
2025-07-26 20:46:48 +0600

    Added todo list in dashboard

--- src/components/window/Dashboard.tsx
+++ src/components/window/Dashboard.tsx
@@ -8,8 +8,9 @@ import { useState, useEffect } from "react";
 import toast from "react-hot-toast";
 import { useTrigger } from "../../contexts/TriggerReRender";
 import { Card, CardContent } from "../ui/card";
-import { SaveIcon, WallpaperIcon } from "lucide-react";
+import { ClockIcon, SaveIcon, TextIcon, WallpaperIcon } from "lucide-react";
 import { useTimeCountContext } from "../../contexts/TimeCountContext";
+import DashboardPendingTasks from "../DashboardPendingTasks";
 // import IdleTimeButton from "../IdleTimeButton";
 
 const Dashboard = () => {
@@ -145,8 +146,8 @@ const Dashboard = () => {
   };
 
   return (
-    <div className="p-4 space-y-4">
-      <div className="flex justify-between items-center space-x-8">
+    <div className="p-2 space-y-3">
+      <div className="flex justify-between items-center space-x-8 -mt-6">
         <div>
           <h1 className="text-5xl font-heading tracking-wider text-[#FE4C55]">
             {currentDate.toLocaleString("en-US", { weekday: "long" })}
@@ -180,84 +181,113 @@ const Dashboard = () => {
         </div>
       </div>
       {/* <IdleTimeButton /> */}
-      <Card>
-        <CardContent className="space-y-6 pt-6">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div className="space-y-2">
-              <Label htmlFor="interval-time">Break Time (Minutes)</Label>
-              <Input
-                type="number"
-                id="interval-time"
-                placeholder="Enter the reminder interval"
-                value={interval}
-                onChange={(e) =>
-                  setInterval(Math.max(1, parseInt(e.target.value, 10) || 1))
-                }
-                className="bg-background"
-              />
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="reminder-duration">
-                Break Duration (Seconds)
-              </Label>
-              <Input
-                type="number"
-                id="reminder-duration"
-                placeholder="Enter the reminder duration"
-                value={duration}
-                onChange={(e) =>
-                  setDuration(Math.max(1, parseInt(e.target.value, 10) || 1))
-                }
-                className="bg-background"
-              />
-            </div>
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="reminder-text">Reminder Screen Text</Label>
-            <Input
-              type="text"
-              id="reminder-text"
-              placeholder="Pause! Look into the distance, and best if you walk a bit."
-              value={reminderText}
-              onChange={(e) => setReminderText(e.target.value)}
-              className="bg-background"
-            />
-          </div>
-          <div className="flex justify-between w-full">
-            <div className="flex space-x-4">
-              <Button onClick={handleSave} variant="default">
-                <SaveIcon className="w-5 h-5" />
-                Save Settings
-              </Button>
-              <Button onClick={handleOpenReminderWindow} variant="outline">
-                <WallpaperIcon className="w-5 h-5" /> Open Reminder Window
-              </Button>
+      {/* Settings Section */}
+      <Card className="bg-black/10 backdrop-blur-3xl">
+        <CardContent className="p-5">
+          <div className="flex space-x-4">
+            <div className="flex flex-col justify-between  space-y-2 border-white/20 border-r pr-4">
+              <div>
+                <Label className="flex items-center gap-2 mb-2">
+                  <ClockIcon className="h-4 w-4" />
+                  Break Frequency
+                </Label>
+                <div className="flex items-center">
+                  <Input
+                    type="number"
+                    value={interval}
+                    onChange={(e) =>
+                      setInterval(
+                        Math.max(1, parseInt(e.target.value, 10) || 1)
+                      )
+                    }
+                    className="bg-background rounded-r-none w-24"
+                  />
+                  <div className="bg-muted px-4 py-2 rounded-r-md text-sm border border-l-0 border-input">
+                    minutes
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <Label className="flex items-center gap-2 mb-2">
+                  <ClockIcon className="h-4 w-4" />
+                  Break Duration
+                </Label>
+                <div className="flex items-center">
+                  <Input
+                    type="number"
+                    value={duration}
+                    onChange={(e) =>
+                      setDuration(
+                        Math.max(1, parseInt(e.target.value, 10) || 1)
+                      )
+                    }
+                    className="bg-background rounded-r-none w-24"
+                  />
+                  <div className="bg-muted px-4 py-2 rounded-r-md text-sm border border-l-0 border-input">
+                    seconds
+                  </div>
+                </div>
+              </div>
             </div>
 
-            <a
-              href="http://github.com/nomandhoni-cs/blink-eye"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center ml-auto"
-            >
-              <div className="flex h-8 w-8 items-center justify-center space-x-1">
-                <svg
-                  role="img"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 fill-current text-black dark:text-white mr-1"
-                >
-                  <title>GitHub</title>
-                  <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
-                </svg>
+            <div className="space-y-4 w-full flex flex-col justify-between">
+              <div>
+                <Label className="flex items-center gap-2 mb-2">
+                  <TextIcon className="h-4 w-4" />
+                  Reminder Message
+                </Label>
+                <Input
+                  type="text"
+                  placeholder="Pause! Look into the distance, and best if you walk a bit."
+                  value={reminderText}
+                  onChange={(e) => setReminderText(e.target.value)}
+                  className="bg-background"
+                />
               </div>
-              <span className="ml-1 text-lg text-muted-foreground">
-                v{appVersion}
-              </span>
-            </a>
+
+              <div className="flex space-x-4 pt-2">
+                <Button onClick={handleSave} className="flex-1">
+                  <SaveIcon className="w-4 h-4 mr-2" />
+                  Save Settings
+                </Button>
+                <Button
+                  onClick={handleOpenReminderWindow}
+                  variant="outline"
+                  className="flex-1"
+                >
+                  <WallpaperIcon className="w-4 h-4 mr-2" />
+                  Preview
+                </Button>
+                <div>
+                  <a
+                    href="http://github.com/nomandhoni-cs/blink-eye"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center ml-auto"
+                  >
+                    <div className="flex h-8 w-8 items-center justify-center space-x-1">
+                      <svg
+                        role="img"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6 fill-current text-black dark:text-white mr-1"
+                      >
+                        <title>GitHub</title>
+                        <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+                      </svg>
+                    </div>
+                    <span className="ml-1 text-lg text-muted-foreground">
+                      v{appVersion}
+                    </span>
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         </CardContent>
       </Card>
+      <DashboardPendingTasks />
     </div>
   );
 };
