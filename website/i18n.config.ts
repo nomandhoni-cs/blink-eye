c5bf0a4b2cb79cff6588b7c79cf3338c8d94847c
Noman Dhoni <alnoman.dhoni@gmail.com>
2026-02-16 16:22:13 +0600

    fix: use --skipNextBuild flag for OpenNext

--- website/i18n.config.ts
+++
@@ -1,8 +0,0 @@
-import { getRequestConfig } from "next-intl/server";
-import { locales } from "@/configs/site";
-
-export default getRequestConfig(async ({ locale }) => {
-  return {
-    messages: (await import(`./messages/${locale}.json`)).default,
-  };
-});
